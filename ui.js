60bb3c617577ff30eab5fbd1d02510f688a32465
Amirmahdi <amirmahdigfx@aut.ac.ir>
2021-01-29 18:54:01 +0330

    adding showNews and RemoveNews

--- ui.js
+++ ui.js
@@ -15,4 +15,26 @@ class UI{
         document.querySelector(".p").innerHTML = "";
         msg.style.display = "none";
     }
+    showNews(news){
+        console.log(news)
+        let result = document.querySelector(".result");
+        news.forEach(element => {
+            const title = element.title.split("-");
+            const description = element.description;
+            const url = element.url;
+            result.innerHTML +=`
+             <div class="item">
+                <div class="item-title">
+                    <h3>${title}</h3>
+                </div>
+                <div class="item-content">${description}</div>
+                <button class="item-btn"><a href="${url}">Full Content</a></button>
+            </div> 
+            `
+        });
+    }
+    removeNews(){
+        let result = document.querySelector(".result");
+        result.innerHTML = "";
+    }
 }
